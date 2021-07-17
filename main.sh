6ba134657a73b78ad2de2f7aec17266932f04e8f
SashiDeveloper <83094605+SashiDeveloper@users.noreply.github.com>
2021-07-17 11:29:07 -0300

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,29 @@
+USERNAME=$REPL_SLUG
+
+VERSION=3.10.2 #Configura a versão desejada, veja em https://github.com/cdr/code-server/releases/
+
+URL=https://github.com/cdr/code-server/releases/download/v$VERSION/code-server-$VERSION-linux-amd64.tar.gz
+
+FILE=/home/runner/$USERNAME/code-server-$VERSION-linux-amd64
+USER_DATA=/home/runner/$USERNAME/user-data/
+
+PASSWORD=SashiDev
+
+export PASSWORD=$PASSWORD
+mkdir $USER_DATA
+
+if [ ! -d "$FILE/" ]
+then
+    wget $URL
+    tar -vzxf $FILE.tar.gz
+    echo "  Nome da repl: $USERNAME 😎  
+    Versão desejada: $VERSION 🥵  
+    Senha: $PASSWORD 🕵️‍♂️"
+    $FILE/bin/code-server --user-data-dir $USER_DATA --bind-addr 0.0.0.0:80
+
+else
+    echo "  Nome da repl: $USERNAME 😎  
+    Versão desejada: $VERSION 🥵  
+    Senha: $PASSWORD 🕵️‍♂️"
+    $FILE/bin/code-server --user-data-dir $USER_DATA --bind-addr 0.0.0.0:80
+fi
